f837c0f52163c357e0a8c80d62b38f49433d7673
Arnar Páll Birgisson <arnarp@gmail.com>
2016-12-16 21:33:30 +0000

    Git ignore case false

--- src/app/Shell/actions.ts
+++
@@ -1,7 +0,0 @@
-import { IUnknownAction } from 'store';
-
-export interface IToggleMenuAction {
-  type: 'shell/TOGGLE_MENU';
-}
-
-export type IShellAction = IUnknownAction | IToggleMenuAction;
--- src/app/Shell/state.ts
+++
@@ -1,4 +0,0 @@
-export interface IShellState {
-  isMenuToggled: boolean;
-  hasMenuBeenToggled: boolean;
-};
