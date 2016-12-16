9bc78db440f3c1a8c3c6026c052605abbd38c90f
Arnar Páll Birgisson <arnarp@gmail.com>
2016-12-16 21:41:39 +0000

    Casing issue. Updated travis build file

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import { IndexRoute, Route } from 'react-router';
-import { Shell } from './app/Shell';
-import { Home } from './app/Home';
-import { About } from './app/About';
+import { Shell } from './app/shell';
+import { Home } from './app/home';
+import { About } from './app/about';
 import { Veislur } from 'app/veislur';
 
 export const routes = (
