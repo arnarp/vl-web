49b572514a02a99798a2f6cca14413bf4e670b84
Arnar Páll Birgisson <arnarp@gmail.com>
2016-12-23 13:42:03 +0000

    Lagaði build

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,4 +2,4 @@ export { HeaderImage } from './HeaderImage';
 export { Hero } from './Hero';
 export { HorizontalDivider } from './HorizontalDivider';
 export { H1 } from './H1';
-export { H2 } from './h2';
+export { H2 } from './H2';
